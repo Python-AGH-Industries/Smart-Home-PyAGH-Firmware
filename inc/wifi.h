31f5229bb42d3ef633e7ef96279122f125984321
Adrian Suliga <adriansuliga81@gmail.com>
2025-04-25 00:49:37 +0200

    Builds, does not want to connect to broker

--- inc/wifi.h
+++ inc/wifi.h
@@ -1,6 +1,9 @@
 #ifndef WIFI_H
 #define WIFI_H
 
+#define CONFIG_MQTT_BROKER_HOSTNAME "mqtt.hivemq.com"
+#define CONFIG_MQTT_PUB_TOPIC "pyagh/smarthome/firmware/sensors"
+
 void wifi_thread(void);
 
 #endif /* WIFI_H */
--- src/wifi.c
+++ src/wifi.c
@@ -1,8 +1,14 @@
 #include "../inc/wifi.h"
 #include <zephyr/kernel.h>
+#include <zephyr/types.h>
+#include <zephyr/random/random.h>
+
 #include <zephyr/net/net_mgmt.h>
 #include <zephyr/net/wifi_mgmt.h>
+#include <zephyr/net/socket.h>
 #include <net/wifi_credentials.h>
+#include <net/mqtt_helper.h>
+
 #include <zephyr/logging/log.h>
 
 K_THREAD_DEFINE(wifi, 2048, wifi_thread, NULL, NULL, NULL, 1, 0, 0);
@@ -11,6 +17,7 @@ LOG_MODULE_REGISTER(wifi_logger);
 #define EVENT_MASK (NET_EVENT_L4_CONNECTED | NET_EVENT_L4_DISCONNECTED)
 
 static struct net_mgmt_event_callback mgmt_cb;
+static uint8_t client_id[sizeof(CONFIG_BOARD) + 11];
 
 static bool connected;
 static K_SEM_DEFINE(run_app, 0, 1);
@@ -34,11 +41,61 @@ static void net_mgmt_event_handler(struct net_mgmt_event_callback *cb,
             LOG_INF("Waiting for wifi to be connected");
         } else {
             LOG_INF("Network disconnected");
+            mqtt_helper_disconnect();
             connected = false;
+            k_sem_reset(&run_app);
         }
-        k_sem_reset(&run_app);
         return;
     }
+}
+
+static int publish(uint8_t* data, size_t len)
+{
+    int err;
+
+    struct mqtt_publish_param mqtt_param;
+
+    mqtt_param.message.payload.data = data;
+    mqtt_param.message.payload.len = len;
+    mqtt_param.message.topic.qos = MQTT_QOS_1_AT_LEAST_ONCE;
+    mqtt_param.message_id = mqtt_helper_msg_id_get();
+    mqtt_param.message.topic.topic.utf8 = CONFIG_MQTT_PUB_TOPIC;
+    mqtt_param.message.topic.topic.size = sizeof(CONFIG_MQTT_PUB_TOPIC) - 1;
+    mqtt_param.dup_flag = 0;
+    mqtt_param.retain_flag = 0;
+
+    err = mqtt_helper_publish(&mqtt_param);
+    if (err) {
+        LOG_WRN("Failed to send payload, err: %d", err);
+        return err;
+    }
+
+    LOG_INF("Published message: \"%.*s\" on topic: \"%.*s\"", 
+        mqtt_param.message.payload.len,
+        mqtt_param.message.payload.data,
+        mqtt_param.message.topic.topic.size,
+        mqtt_param.message.topic.topic.utf8
+    );
+
+    return 0;
+}
+
+static void on_mqtt_connack(enum mqtt_conn_return_code return_code, bool session_present)
+{
+    if (return_code == MQTT_CONNECTION_ACCEPTED) {
+        LOG_INF("Connected to MQTT broker");
+        LOG_INF("Hostname: %s", CONFIG_MQTT_BROKER_HOSTNAME);
+		LOG_INF("Client ID: %s", (char *)client_id);
+		LOG_INF("Port: %d", CONFIG_MQTT_HELPER_PORT);
+		LOG_INF("TLS: %s", IS_ENABLED(CONFIG_MQTT_LIB_TLS) ? "Yes" : "No");
+    } else {
+        LOG_WRN("Connection to broker not established, err = %d", return_code);
+    }
+}
+
+static void on_mqtt_disconnect(int result)
+{
+    LOG_INF("Disconnect from the broker, result = %d", result);
 }
 
 void wifi_thread(void)
@@ -60,7 +117,37 @@ void wifi_thread(void)
     while (true) {
         k_sem_take(&run_app, K_FOREVER);
         while (k_sem_count_get(&run_app) == 0) {
-            LOG_INF("Wi-Fi is ready");
+            struct mqtt_helper_cfg config = {
+                .cb = {
+                    .on_connack = on_mqtt_connack,
+                    .on_disconnect = on_mqtt_disconnect
+                }
+            };
+
+            int err = mqtt_helper_init(&config);
+            if (err) {
+                LOG_ERR("Failed to initialize MQTT helper, error: %d", err);
+                k_sleep(K_SECONDS(1));
+                continue;
+            }
+
+            uint32_t id = sys_rand32_get();
+            snprintf(client_id, sizeof(client_id), "%s-%010u", CONFIG_BOARD, id);
+
+            struct mqtt_helper_conn_params conn_params = {
+                .hostname.ptr = CONFIG_MQTT_BROKER_HOSTNAME,
+                .hostname.size = sizeof(CONFIG_MQTT_BROKER_HOSTNAME) - 1,
+                .device_id.ptr = (char *)client_id,
+                .device_id.size = sizeof(client_id) - 1
+            };
+
+            err = mqtt_helper_connect(&conn_params);
+            if (err) {
+                LOG_ERR("Failed to connect to MQTT, error code: %d", err);
+                k_sleep(K_SECONDS(1));
+                continue;
+            }
+
             k_sleep(K_SECONDS(1));
         }
         LOG_INF("App stoped");
