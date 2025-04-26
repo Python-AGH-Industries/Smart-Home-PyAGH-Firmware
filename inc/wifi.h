bee49f6f6a0bf121dd890a1a9b507da3cc2beb9d
Adrian Suliga <adriansuliga81@gmail.com>
2025-04-26 13:06:25 +0200

    Fix mqtt - it connects now!

--- inc/wifi.h
+++ inc/wifi.h
@@ -1,8 +1,11 @@
 #ifndef WIFI_H
 #define WIFI_H
 
-#define CONFIG_MQTT_BROKER_HOSTNAME "mqtt.hivemq.com"
-#define CONFIG_MQTT_PUB_TOPIC "pyagh/smarthome/firmware/sensors"
+#define SENSOR_PUBLISH_TOPIC "pyagh/smarthome/sensors/data"
+
+#define BROKER_HOSTNAME "broker.hivemq.com"
+
+#define WIFI_STACK_SIZE 2048
 
 void wifi_thread(void);
 
--- src/wifi.c
+++ src/wifi.c
@@ -1,4 +1,5 @@
 #include "../inc/wifi.h"
+
 #include <zephyr/kernel.h>
 #include <zephyr/types.h>
 #include <zephyr/random/random.h>
@@ -6,12 +7,13 @@
 #include <zephyr/net/net_mgmt.h>
 #include <zephyr/net/wifi_mgmt.h>
 #include <zephyr/net/socket.h>
+
 #include <net/wifi_credentials.h>
 #include <net/mqtt_helper.h>
 
 #include <zephyr/logging/log.h>
 
-K_THREAD_DEFINE(wifi, 2048, wifi_thread, NULL, NULL, NULL, 1, 0, 0);
+K_THREAD_DEFINE(wifi, WIFI_STACK_SIZE, wifi_thread, NULL, NULL, NULL, 1, 0, 0);
 LOG_MODULE_REGISTER(wifi_logger);
 
 #define EVENT_MASK (NET_EVENT_L4_CONNECTED | NET_EVENT_L4_DISCONNECTED)
@@ -21,6 +23,7 @@ static uint8_t client_id[sizeof(CONFIG_BOARD) + 11];
 
 static bool connected;
 static K_SEM_DEFINE(run_app, 0, 1);
+static K_SEM_DEFINE(mqtt_connected, 0, 1);
 
 static void net_mgmt_event_handler(struct net_mgmt_event_callback *cb, 
                                 uint32_t mgmt_event, struct net_if *iface)
@@ -28,7 +31,7 @@ static void net_mgmt_event_handler(struct net_mgmt_event_callback *cb,
     if ((mgmt_event & EVENT_MASK) != mgmt_event) {
         return;
     }
-
+    
     if (mgmt_event == NET_EVENT_L4_CONNECTED) {
         LOG_INF("Network connected");
         connected = true;
@@ -59,8 +62,8 @@ static int publish(uint8_t* data, size_t len)
     mqtt_param.message.payload.len = len;
     mqtt_param.message.topic.qos = MQTT_QOS_1_AT_LEAST_ONCE;
     mqtt_param.message_id = mqtt_helper_msg_id_get();
-    mqtt_param.message.topic.topic.utf8 = CONFIG_MQTT_PUB_TOPIC;
-    mqtt_param.message.topic.topic.size = sizeof(CONFIG_MQTT_PUB_TOPIC) - 1;
+    mqtt_param.message.topic.topic.utf8 = SENSOR_PUBLISH_TOPIC;
+    mqtt_param.message.topic.topic.size = sizeof(SENSOR_PUBLISH_TOPIC) - 1,
     mqtt_param.dup_flag = 0;
     mqtt_param.retain_flag = 0;
 
@@ -84,10 +87,11 @@ static void on_mqtt_connack(enum mqtt_conn_return_code return_code, bool session
 {
     if (return_code == MQTT_CONNECTION_ACCEPTED) {
         LOG_INF("Connected to MQTT broker");
-        LOG_INF("Hostname: %s", CONFIG_MQTT_BROKER_HOSTNAME);
+        LOG_INF("Hostname: %s", BROKER_HOSTNAME);
 		LOG_INF("Client ID: %s", (char *)client_id);
 		LOG_INF("Port: %d", CONFIG_MQTT_HELPER_PORT);
 		LOG_INF("TLS: %s", IS_ENABLED(CONFIG_MQTT_LIB_TLS) ? "Yes" : "No");
+        k_sem_give(&mqtt_connected);
     } else {
         LOG_WRN("Connection to broker not established, err = %d", return_code);
     }
@@ -96,6 +100,7 @@ static void on_mqtt_connack(enum mqtt_conn_return_code return_code, bool session
 static void on_mqtt_disconnect(int result)
 {
     LOG_INF("Disconnect from the broker, result = %d", result);
+    k_sem_reset(&mqtt_connected);
 }
 
 void wifi_thread(void)
@@ -135,8 +140,8 @@ void wifi_thread(void)
             snprintf(client_id, sizeof(client_id), "%s-%010u", CONFIG_BOARD, id);
 
             struct mqtt_helper_conn_params conn_params = {
-                .hostname.ptr = CONFIG_MQTT_BROKER_HOSTNAME,
-                .hostname.size = sizeof(CONFIG_MQTT_BROKER_HOSTNAME) - 1,
+                .hostname.ptr = BROKER_HOSTNAME,
+                .hostname.size = sizeof(BROKER_HOSTNAME) - 1,
                 .device_id.ptr = (char *)client_id,
                 .device_id.size = sizeof(client_id) - 1
             };
@@ -148,7 +153,12 @@ void wifi_thread(void)
                 continue;
             }
 
-            k_sleep(K_SECONDS(1));
+            k_sem_take(&mqtt_connected, K_FOREVER);
+
+            while (k_sem_count_get(&mqtt_connected) == 0) {
+                LOG_INF("Mqtt connection established, waiting for sensor data");
+                k_sleep(K_SECONDS(1));
+            }
         }
         LOG_INF("App stoped");
     }
